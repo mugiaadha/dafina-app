67df22ade3060dc64bfc4e2baef2a2a64d8dd0fc
LAPTOP-00IGEHMU\mugia <mugiaadha@gmail.com>
2025-06-05 17:44:12 +0700

    update

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent implements OnInit {
-
-  constructor() { }
+  blog: any;
+  constructor(private titleService: Title, private metaService: Meta) {}
 
   ngOnInit(): void {
-  }
+    this.blog = {
+      title:
+        'Dafina Salon Bogor - Salon terbaik di Bogor dan Salon termurah di Bogor',
+      description: 'salon bukit cimanggu, salon wanita bogor',
+    };
 
+    this.titleService.setTitle(this.blog.title);
+    this.metaService.updateTag({
+      name: 'description',
+      content: this.blog.description,
+    });
+  }
 }
